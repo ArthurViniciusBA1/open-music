9fdcb8cc710eb9b7bee5934221cba02d729b6ab2
Arthur Vinícius <arthurnaocopia@gmail.com>
2022-10-05 00:49:03 -0300

    entrega finalizada

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,103 @@
 /* Desenvolva sua lógica aqui ... */
+
+
+
+
+categories.forEach(element => {
+    let listaCategorias = document.querySelector("#listaCategorias")
+
+    let input = document.createElement("input")
+    let label = document.createElement("label")
+
+    id = padronizarString(element)
+
+    input.type = "radio"
+    input.name = "estilosMusicais"
+    input.id = `input_${id}`
+    input.hidden = true
+    input.className = "radio"
+
+    label.htmlFor = `input_${id}`
+    label.className = "filtroEstilos"
+    label.id = id
+    label.innerText = element
+
+    if (id === "todos") {
+        input.checked = "true"
+    }
+
+    listaCategorias.append(input, label)
+})
+
+const botoesFiltroMusicas = [...document.getElementsByClassName("filtroEstilos")]
+const listaAlbuns = document.getElementById("listaAlbuns")
+let radio = [...document.getElementsByClassName("radio")]
+const range = document.getElementById("range")
+let filtro = "todos"
+
+gerarAlbuns()
+
+range.addEventListener('mouseup', () => {
+    gerarAlbuns()
+})
+
+botoesFiltroMusicas.forEach(element => {
+    element.addEventListener("click", (event) => {
+        filtro = event.target.id
+        gerarAlbuns()
+    })
+});
+
+function gerarAlbuns() {
+    listaAlbuns.innerHTML = ""
+
+    let valorMaximo = range.value
+    let produtosFiltradosPorPreco = products.filter(object => object.price <= valorMaximo)
+
+    let category = categories.map(padronizarString).indexOf(filtro)
+
+    if (filtro === "todos") {
+        produtosFiltradosPorPreco.forEach(element => {
+            listaAlbuns.append(gerarLi(element))
+        })
+    } else {
+        produtosFiltradosPorPreco.filter(object => object.category === category).forEach(element => {
+            listaAlbuns.append(gerarLi(element))
+        })
+    }
+
+
+}
+
+function gerarLi(objeto) {
+    let li = document.createElement("li")
+
+    let valorConvertido = objeto.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+
+
+    li.insertAdjacentHTML("afterbegin", `
+    <img src="${objeto.img}">
+    <div class="conteudo">
+        <div class="informacoes">
+            <span>${objeto.band}</span>
+            <span>${objeto.year}</span>
+        </div>
+        <h3>${objeto.title}</h3>
+        <div class="comprar">
+            <span>${valorConvertido}</span>
+            <button type="button">Comprar</button>
+        </div>
+    </div>
+    `)
+
+    li.id = `album_${objeto.id}`
+
+    return li
+}
+
+function padronizarString(elemento) {
+    return elemento.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+}
+
+
+
--- scripts/theme.js
+++ scripts/theme.js
@@ -1,1 +1,41 @@
 /* Desenvolva sua lógica aqui ... */
+darkMode()
+
+const botaoDm = document.getElementById("darkMode")
+
+botaoDm.addEventListener("click", (event) => {
+    darkMode(1)
+})
+
+function darkMode(click) {
+    const html = document.querySelector("html")
+    let icone = document.getElementById("darkModeIcon")
+    const preferenciaDm = localStorage.getItem("darkMode")
+
+    if (click === 1) {
+        html.classList.toggle("darkMode")
+        icone.classList.toggle("textWhite")
+        if (!preferenciaDm) {
+            localStorage.setItem("darkMode", true)
+        }
+    
+        if (preferenciaDm) {
+            localStorage.removeItem("darkMode")
+        }
+    
+        if (icone.classList.contains("textWhite")) {
+            icone.src = "../../assets/sun.svg"
+        } else {
+            icone.src = "../../assets/moon.svg"
+        }
+        return
+    }
+
+   
+
+    if (preferenciaDm) {
+        html.classList.toggle("darkMode")
+        icone.classList.toggle("textWhite")
+        icone.src = "../../assets/sun.svg"
+    }
+}
